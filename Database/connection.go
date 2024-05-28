71bc3b123c724d24b26d4d7a1fc1257d35a29ad4
lucas-martins <lucasmartinsnara@gmail.com>
2024-05-28 12:54:06 -0300

    open a database connection

---
+++ Database/connection.go
@@ -0,0 +1,48 @@
+package Database
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/Lucasmartinsn/grocery-api/Configs"
+
+	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
+)
+
+func connectionType() (driver, sc string) {
+	conf := Configs.GetDB()
+
+	switch conf.Driver {
+	case "postgres":
+		driver = "postgres"
+		sc = fmt.Sprintf(
+			"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+			conf.Host, conf.Port, conf.User, conf.Pass, conf.Database,
+		)
+	case "mysql":
+		driver = "mysql"
+		sc = fmt.Sprintf(
+			"%s:%s@tcp(%s:%s)/%s",
+			conf.User, conf.Pass, conf.Host, conf.Port, conf.Database,
+		)
+	default:
+		log.Fatalf("Driver desconhecido: %s", conf.Driver)
+	}
+	return
+}
+
+func OpenConnection() (*sql.DB, error) {
+	driver, sc := connectionType()
+	
+	// Open Connection
+	conn, err := sql.Open(driver, sc)
+	if err != nil {
+		log.Fatalf("error database connection: %v", err)
+	}
+
+	// Testing connection
+	err = conn.Ping()
+	return conn, err
+}
